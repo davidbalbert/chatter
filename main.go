d20798a54258051aaeb2f6ed45de316490ae9ae8
David Albert <davidbalbert@gmail.com>
2022-11-30 22:05:16 -0500

    Collect utility funcs

--- main.go
+++ main.go
@@ -21,6 +21,19 @@ func toNetAddr(addr netip.Addr) net.Addr {
 	return &net.IPAddr{IP: addr.AsSlice()}
 }
 
+func to4(addr netip.Addr) []byte {
+	b := addr.As4()
+	return b[:]
+}
+
+func mustAddrFromSlice(b []byte) netip.Addr {
+	addr, ok := netip.AddrFromSlice(b)
+	if !ok {
+		panic("mustAddrFromSlice: slice should be either 4 or 16 bytes, but got " + fmt.Sprint(len(b)))
+	}
+	return addr
+}
+
 type messageType uint8
 
 const (
@@ -77,19 +90,6 @@ type Packet struct {
 	Header
 	Content any
 	data    []byte
-}
-
-func mustAddrFromSlice(b []byte) netip.Addr {
-	addr, ok := netip.AddrFromSlice(b)
-	if !ok {
-		panic("mustAddrFromSlice: slice should be either 4 or 16 bytes, but got " + fmt.Sprint(len(b)))
-	}
-	return addr
-}
-
-func to4(addr netip.Addr) []byte {
-	b := addr.As4()
-	return b[:]
 }
 
 func decodePacket(data []byte) (*Packet, error) {
