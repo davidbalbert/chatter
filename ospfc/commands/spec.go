73da6277358ccae921430355171d362c143839c4
David Albert <davidbalbert@gmail.com>
2023-04-01 13:14:14 -0400

    Simple Spec parser for making assertions on graphs

---
+++ ospfc/commands/spec.go
@@ -0,0 +1,273 @@
+package commands
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+// An example spec:
+// fork[
+//	  	literal:foo[
+//			join.1
+//		],
+//		literal:bar[
+//			join.1
+//		]
+// ]
+//
+// Whitespace between children is not significant. A rough grammar is:
+//
+// spec <- ws? name id? children? ws?
+// name <- "fork" / "join" / "literal:" word / "argument:" argtype
+// id <- '.' [1-9][0-9]*
+// children <- '[' ws? spec (ws? ',' ws? spec)* ws? ']'
+// word <- [a-zA-Z0-9]+
+// argtype <- "string" / "ipv4" / "ipv6"
+// ws <- [ \t\r\n]*
+
+type spec struct {
+	typeName string
+	arg      string
+	id       int
+	children []*spec
+}
+
+type specParser struct {
+	s    string
+	pos  int
+	line int
+	col  int
+}
+
+func (p *specParser) parse() (*spec, error) {
+	s := &spec{}
+
+	p.skipWhitespace()
+
+	if err := p.parseName(s); err != nil {
+		return nil, err
+	}
+
+	if p.peek() == '.' {
+		if err := p.parseID(s); err != nil {
+			return nil, err
+		}
+	}
+
+	if p.peek() == '[' {
+		if err := p.parseChildren(s); err != nil {
+			return nil, err
+		}
+	}
+
+	p.skipWhitespace()
+
+	return s, nil
+}
+
+func (p *specParser) parseName(s *spec) error {
+	if p.peek() == 'f' {
+		if !p.consume("fork") {
+			return p.errorf("expected 'fork'")
+		}
+
+		s.typeName = "fork"
+		return nil
+	}
+
+	if p.peek() == 'j' {
+		if !p.consume("join") {
+			return p.errorf("expected 'join'")
+		}
+
+		s.typeName = "join"
+		return nil
+	}
+
+	if p.peek() == 'l' {
+		if !p.consume("literal:") {
+			return p.errorf("expected 'literal:'")
+		}
+
+		s.typeName = "literal"
+		if err := p.parseWord(s); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	if p.peek() == 'a' {
+		if !p.consume("argument:") {
+			return p.errorf("expected 'argument:'")
+		}
+
+		s.typeName = "argument"
+		if err := p.parseArgType(s); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	return p.errorf("expected 'fork', 'join', 'literal:', or 'argument:'")
+}
+
+func (p *specParser) parseID(s *spec) error {
+	p.next() // consume the '.'
+
+	var num string
+	if p.peek() == '0' {
+		return p.errorf("invalid id 0")
+	} else if p.peek() < '1' || p.peek() > '9' {
+		return p.errorf("expected digit")
+	}
+
+	for p.peek() >= '0' && p.peek() <= '9' {
+		num += string(p.next())
+	}
+
+	id, err := strconv.Atoi(num)
+	if err != nil {
+		return p.errorf("invalid id %s", num)
+	}
+
+	s.id = id
+
+	return nil
+}
+
+func (p *specParser) parseChildren(s *spec) error {
+	if p.next() != '[' {
+		return p.errorf("expected '['")
+	}
+
+	p.skipWhitespace()
+
+	for p.peek() != ']' {
+		child, err := p.parse()
+		if err != nil {
+			return err
+		}
+
+		s.children = append(s.children, child)
+
+		p.skipWhitespace()
+
+		if p.peek() == ',' {
+			p.next()
+			p.skipWhitespace()
+		}
+	}
+
+	if p.next() != ']' {
+		return p.errorf("expected ']'")
+	}
+
+	return nil
+}
+
+func (p *specParser) parseWord(s *spec) error {
+	var word string
+	for p.peek() >= 'a' && p.peek() <= 'z' || p.peek() >= 'A' && p.peek() <= 'Z' || p.peek() >= '0' && p.peek() <= '9' {
+		word += string(p.next())
+	}
+
+	if word == "" {
+		return p.errorf("expected word")
+	}
+
+	s.arg = word
+
+	return nil
+}
+
+func (p *specParser) parseArgType(s *spec) error {
+	if p.peek() == 's' {
+		if !p.consume("string") {
+			return p.errorf("expected 'string'")
+		}
+
+		s.arg = "string"
+		return nil
+	}
+
+	if p.peek() == 'i' {
+		if !p.consume("ipv") {
+			return p.errorf("expected 'ipv4' or 'ipv6'")
+		}
+
+		if p.peek() == '4' {
+			p.next()
+			s.arg = "ipv4"
+			return nil
+		}
+
+		if p.peek() == '6' {
+			p.next()
+			s.arg = "ipv6"
+			return nil
+		}
+
+		return p.errorf("expected 'ipv4' or 'ipv6'")
+	}
+
+	return p.errorf("expected 'string', 'ipv4', or 'ipv6'")
+}
+
+func (p *specParser) skipWhitespace() {
+	for p.peek() == ' ' || p.peek() == '\t' || p.peek() == '\r' || p.peek() == '\n' {
+		p.next()
+	}
+}
+
+func (p *specParser) peek() rune {
+	r, _ := utf8.DecodeRuneInString(p.s[p.pos:])
+
+	return r
+}
+
+func (p *specParser) next() rune {
+	r, size := utf8.DecodeRuneInString(p.s[p.pos:])
+	p.pos += size
+
+	if r == '\n' {
+		p.line++
+		p.col = 0
+	} else {
+		p.col++
+	}
+
+	return r
+}
+
+func (p *specParser) consume(s string) bool {
+	if !strings.HasPrefix(p.s[p.pos:], s) {
+		return false
+	}
+
+	for range s {
+		p.next()
+	}
+
+	return true
+}
+
+func (p *specParser) errorf(format string, args ...interface{}) error {
+	lines := strings.Split(p.s, "\n")
+	line := lines[p.line-1]
+	marker := strings.Repeat(" ", p.col) + "^"
+
+	return fmt.Errorf("%d:%d: %s\n\t%s\n\t%s", p.line, p.col, fmt.Sprintf(format, args...), line, marker)
+}
+
+func parseSpec(s string) (*spec, error) {
+	p := &specParser{
+		s:    s,
+		line: 1,
+	}
+
+	return p.parse()
+}
---
+++ ospfc/commands/spec_test.go
@@ -0,0 +1,509 @@
+package commands
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseFork(t *testing.T) {
+	spec, err := parseSpec("fork")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec.typeName != "fork" {
+		t.Fatalf("expected fork, got %q", spec.typeName)
+	}
+
+	if spec.arg != "" {
+		t.Fatalf("expected empty arg, got %q", spec.arg)
+	}
+
+	if len(spec.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(spec.children))
+	}
+
+	if spec.id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.id)
+	}
+}
+
+func TestParseJoin(t *testing.T) {
+	spec, err := parseSpec("join")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec.typeName != "join" {
+		t.Fatalf("expected join, got %q", spec.typeName)
+	}
+
+	if spec.arg != "" {
+		t.Fatalf("expected empty arg, got %q", spec.arg)
+	}
+
+	if len(spec.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(spec.children))
+	}
+
+	if spec.id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.id)
+	}
+}
+
+func TestParseJoinWithID(t *testing.T) {
+	spec, err := parseSpec("join.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec.typeName != "join" {
+		t.Fatalf("expected join, got %q", spec.typeName)
+	}
+
+	if spec.arg != "" {
+		t.Fatalf("expected empty arg, got %q", spec.arg)
+	}
+
+	if len(spec.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(spec.children))
+	}
+
+	if spec.id != 1 {
+		t.Fatalf("expected id 1, got %d", spec.id)
+	}
+}
+
+func TestParseLiteral(t *testing.T) {
+	spec, err := parseSpec("literal:foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec.typeName != "literal" {
+		t.Fatalf("expected literal, got %q", spec.typeName)
+	}
+
+	if spec.arg != "foo" {
+		t.Fatalf("expected arg foo, got %q", spec.arg)
+	}
+
+	if len(spec.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(spec.children))
+	}
+
+	if spec.id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.id)
+	}
+}
+
+func TestParseLiteralMissingValue(t *testing.T) {
+	_, err := parseSpec("literal")
+	if err == nil {
+		t.Fatal("expected error")
+	}
+
+	_, err = parseSpec("literal:")
+	if err == nil {
+		t.Fatal("expected error")
+	}
+}
+
+func TestParseArgumentString(t *testing.T) {
+	spec, err := parseSpec("argument:string")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec.typeName != "argument" {
+		t.Fatalf("expected argument, got %q", spec.typeName)
+	}
+
+	if spec.arg != "string" {
+		t.Fatalf("expected arg foo, got %q", spec.arg)
+	}
+
+	if len(spec.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(spec.children))
+	}
+
+	if spec.id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.id)
+	}
+}
+
+func TestParseArgumentIPv4(t *testing.T) {
+	spec, err := parseSpec("argument:ipv4")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec.typeName != "argument" {
+		t.Fatalf("expected argument, got %q", spec.typeName)
+	}
+
+	if spec.arg != "ipv4" {
+		t.Fatalf("expected arg foo, got %q", spec.arg)
+	}
+
+	if len(spec.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(spec.children))
+	}
+
+	if spec.id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.id)
+	}
+}
+
+func TestParseArgumentIPv6(t *testing.T) {
+	spec, err := parseSpec("argument:ipv6")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec.typeName != "argument" {
+		t.Fatalf("expected argument, got %q", spec.typeName)
+	}
+
+	if spec.arg != "ipv6" {
+		t.Fatalf("expected arg foo, got %q", spec.arg)
+	}
+
+	if len(spec.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(spec.children))
+	}
+
+	if spec.id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.id)
+	}
+}
+
+func TestParseArgumentBadType(t *testing.T) {
+	_, err := parseSpec("argument:foo")
+	if err == nil {
+		t.Fatal("expected error")
+	}
+}
+
+func TestParseArgumentMissingType(t *testing.T) {
+	_, err := parseSpec("argument")
+	if err == nil {
+		t.Fatal("expected error")
+	}
+
+	_, err = parseSpec("argument:")
+	if err == nil {
+		t.Fatal("expected error")
+	}
+}
+
+func TestLiteralWithChild(t *testing.T) {
+	s, err := parseSpec("literal:foo[literal:bar]")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if s == nil {
+		t.Fatal("expected spec")
+	}
+
+	if s.typeName != "literal" {
+		t.Fatalf("expected literal, got %q", s.typeName)
+	}
+
+	if s.arg != "foo" {
+		t.Fatalf("expected arg foo, got %q", s.arg)
+	}
+
+	if s.id != 0 {
+		t.Fatalf("expected id 0, got %d", s.id)
+	}
+
+	if len(s.children) != 1 {
+		t.Fatalf("expected 1 child, got %d", len(s.children))
+	}
+
+	if s.children[0].typeName != "literal" {
+		t.Fatalf("expected literal, got %q", s.children[0].typeName)
+	}
+
+	if s.children[0].arg != "bar" {
+		t.Fatalf("expected arg bar, got %q", s.children[0].arg)
+	}
+
+	if s.children[0].id != 0 {
+		t.Fatalf("expected id 0, got %d", s.children[0].id)
+	}
+
+	if len(s.children[0].children) != 0 {
+		t.Fatalf("expected no children, got %d", len(s.children[0].children))
+	}
+}
+
+func TestLiteralWithChildAndWhitespace(t *testing.T) {
+	s := `
+	literal:foo[
+		literal:bar
+	]
+	`
+
+	spec, err := parseSpec(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec == nil {
+		t.Fatal("expected spec")
+	}
+
+	if spec.typeName != "literal" {
+		t.Fatalf("expected literal, got %q", spec.typeName)
+	}
+
+	if spec.arg != "foo" {
+		t.Fatalf("expected arg foo, got %q", spec.arg)
+	}
+
+	if spec.id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.id)
+	}
+
+	if len(spec.children) != 1 {
+		t.Fatalf("expected 1 child, got %d", len(spec.children))
+	}
+
+	if spec.children[0].typeName != "literal" {
+		t.Fatalf("expected literal, got %q", spec.children[0].typeName)
+	}
+
+	if spec.children[0].arg != "bar" {
+		t.Fatalf("expected arg bar, got %q", spec.children[0].arg)
+	}
+
+	if spec.children[0].id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.children[0].id)
+	}
+
+	if len(spec.children[0].children) != 0 {
+		t.Fatalf("expected no children, got %d", len(spec.children[0].children))
+	}
+}
+
+func TestForkWithChildren(t *testing.T) {
+	s := `
+	fork[
+		literal:all,
+		argument:ipv4,
+		argument:ipv6,
+		argument:string,
+	]
+	`
+
+	spec, err := parseSpec(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec == nil {
+		t.Fatal("expected spec")
+	}
+
+	if spec.typeName != "fork" {
+		t.Fatalf("expected fork, got %q", spec.typeName)
+	}
+
+	if spec.arg != "" {
+		t.Fatalf("expected arg '', got %q", spec.arg)
+	}
+
+	if spec.id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.id)
+	}
+
+	if len(spec.children) != 4 {
+		t.Fatalf("expected 4 children, got %d", len(spec.children))
+	}
+
+	c1 := spec.children[0]
+	if c1.typeName != "literal" {
+		t.Fatalf("expected literal, got %q", c1.typeName)
+	}
+
+	if c1.arg != "all" {
+		t.Fatalf("expected arg all, got %q", c1.arg)
+	}
+
+	if c1.id != 0 {
+		t.Fatalf("expected id 0, got %d", c1.id)
+	}
+
+	if len(c1.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(c1.children))
+	}
+
+	c2 := spec.children[1]
+	if c2.typeName != "argument" {
+		t.Fatalf("expected argument, got %q", c2.typeName)
+	}
+
+	if c2.arg != "ipv4" {
+		t.Fatalf("expected arg ipv4, got %q", c2.arg)
+	}
+
+	if c2.id != 0 {
+		t.Fatalf("expected id 0, got %d", c2.id)
+	}
+
+	if len(c2.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(c2.children))
+	}
+
+	c3 := spec.children[2]
+	if c3.typeName != "argument" {
+		t.Fatalf("expected argument, got %q", c3.typeName)
+	}
+
+	if c3.arg != "ipv6" {
+		t.Fatalf("expected arg ipv6, got %q", c3.arg)
+	}
+
+	if c3.id != 0 {
+		t.Fatalf("expected id 0, got %d", c3.id)
+	}
+
+	if len(c3.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(c3.children))
+	}
+
+	c4 := spec.children[3]
+	if c4.typeName != "argument" {
+		t.Fatalf("expected argument, got %q", c4.typeName)
+	}
+
+	if c4.arg != "string" {
+		t.Fatalf("expected arg string, got %q", c4.arg)
+	}
+
+	if c4.id != 0 {
+		t.Fatalf("expected id 0, got %d", c4.id)
+	}
+
+	if len(c4.children) != 0 {
+		t.Fatalf("expected no children, got %d", len(c4.children))
+	}
+}
+
+func TestForkJoinWithID(t *testing.T) {
+	s := `
+	fork[
+		literal:all[
+			join.1
+		],
+		argument:ipv4[
+			join.1
+		]
+	]
+	`
+
+	spec, err := parseSpec(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if spec == nil {
+		t.Fatal("expected spec")
+	}
+
+	if spec.typeName != "fork" {
+		t.Fatalf("expected fork, got %q", spec.typeName)
+	}
+
+	if spec.arg != "" {
+		t.Fatalf("expected arg '', got %q", spec.arg)
+	}
+
+	if spec.id != 0 {
+		t.Fatalf("expected id 0, got %d", spec.id)
+	}
+
+	if len(spec.children) != 2 {
+		t.Fatalf("expected 2 children, got %d", len(spec.children))
+	}
+
+	c1 := spec.children[0]
+	if c1.typeName != "literal" {
+		t.Fatalf("expected literal, got %q", c1.typeName)
+	}
+
+	if c1.arg != "all" {
+		t.Fatalf("expected arg all, got %q", c1.arg)
+	}
+
+	if c1.id != 0 {
+		t.Fatalf("expected id 0, got %d", c1.id)
+	}
+
+	if len(c1.children) != 1 {
+		t.Fatalf("expected 1 child, got %d", len(c1.children))
+	}
+
+	if c1.children[0].typeName != "join" {
+		t.Fatalf("expected join, got %q", c1.children[0].typeName)
+	}
+
+	if c1.children[0].arg != "" {
+		t.Fatalf("expected arg '', got %q", c1.children[0].arg)
+	}
+
+	if c1.children[0].id != 1 {
+		t.Fatalf("expected id 1, got %d", c1.children[0].id)
+	}
+
+	if len(c1.children[0].children) != 0 {
+		t.Fatalf("expected no children, got %d", len(c1.children[0].children))
+	}
+
+	c2 := spec.children[1]
+	if c2.typeName != "argument" {
+		t.Fatalf("expected argument, got %q", c2.typeName)
+	}
+
+	if c2.arg != "ipv4" {
+		t.Fatalf("expected arg ipv4, got %q", c2.arg)
+	}
+
+	if c2.id != 0 {
+		t.Fatalf("expected id 0, got %d", c2.id)
+	}
+
+	if len(c2.children) != 1 {
+		t.Fatalf("expected 1 child, got %d", len(c2.children))
+	}
+
+	if c2.children[0].typeName != "join" {
+		t.Fatalf("expected join, got %q", c2.children[0].typeName)
+	}
+
+	if c2.children[0].arg != "" {
+		t.Fatalf("expected arg '', got %q", c2.children[0].arg)
+	}
+
+	if c2.children[0].id != 1 {
+		t.Fatalf("expected id 1, got %d", c2.children[0].id)
+	}
+
+	if len(c2.children[0].children) != 0 {
+		t.Fatalf("expected no children, got %d", len(c2.children[0].children))
+	}
+}
+
+func TestInvalidZeroID(t *testing.T) {
+	_, err := parseSpec("join.0")
+	if err == nil {
+		t.Fatal("expected error")
+	}
+
+	if !strings.Contains(err.Error(), "invalid id 0") {
+		t.Fatalf("expected error to contain 'invalid id 0', got %q", err.Error())
+	}
+}
