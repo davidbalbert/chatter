74af65041fe6656f7442c01fa94ec05b7f033b0a
David Albert <davidbalbert@gmail.com>
2023-04-20 15:02:03 -0400

    Down arrow support + temporarily make public

--- chatterc/cli.go
+++ chatterc/cli.go
@@ -389,7 +389,7 @@ func (cli *CLI) Run(rw readWriteFder) {
 			break
 		}
 
-		p := newPager(int(rw.Fd()), rw, t)
+		p := NewPager(rw, t)
 		cli.runLine(line, p)
 	}
 }
--- chatterc/pager.go
+++ chatterc/pager.go
@@ -4,12 +4,17 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"os"
 	"strings"
 
 	"golang.org/x/term"
 )
 
-type pager struct {
+// A simple pager like more(1). Implements io.Writer. If r is a terminal,
+// it must be put in raw mode with term.MakeRaw(). If r is not a terminal,
+// Pager is a no-op and all output is directly written to w.
+
+type Pager struct {
 	fd      int
 	w       io.Writer
 	r       io.Reader
@@ -19,17 +24,26 @@ type pager struct {
 	stopped bool
 }
 
-func newPager(fd int, r io.Reader, w io.Writer) *pager {
-	return &pager{
+func NewPager(r io.Reader, w io.Writer) *Pager {
+	isTerm := false
+	fd := 0
+
+	f, ok := r.(*os.File)
+	if ok {
+		fd = int(f.Fd())
+		isTerm = term.IsTerminal(fd)
+	}
+
+	return &Pager{
 		fd:     fd,
 		w:      w,
 		r:      r,
 		buf:    bytes.Buffer{},
-		isTerm: term.IsTerminal(fd),
+		isTerm: isTerm,
 	}
 }
 
-func (p *pager) Write(b []byte) (n int, err error) {
+func (p *Pager) Write(b []byte) (n int, err error) {
 	if !p.isTerm {
 		return p.w.Write(b)
 	}
@@ -70,6 +84,19 @@ func (p *pager) Write(b []byte) (n int, err error) {
 				case '\r', 'j':
 					p.line--
 					break ReadChar
+				case '\x1b':
+					// escape sequence, read next two bytes
+					b := make([]byte, 2)
+					_, err := p.r.Read(b)
+					if err != nil {
+						return written, err
+					}
+
+					if b[0] == '[' && b[1] == 'B' {
+						// down arrow
+						p.line--
+						break ReadChar
+					}
 				}
 
 				// invalid character, ring bell
