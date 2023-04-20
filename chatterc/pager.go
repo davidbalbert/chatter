04eaad782565017cda643c0dbc1369731c613c5a
David Albert <davidbalbert@gmail.com>
2023-04-20 18:23:26 -0400

    Refactored pager

--- chatterc/pager.go
+++ chatterc/pager.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
-	"fmt"
 	"io"
-	"os"
 	"strings"
 
 	"golang.org/x/term"
@@ -12,23 +11,24 @@ import (
 
 // A simple pager like more(1). Implements io.Writer. If r is a terminal,
 // it must be put in raw mode with term.MakeRaw(). If r is not a terminal,
-// Pager is a no-op and all output is directly written to w.
-
+// pager is a no-op and all output is directly written to w.
 type pager struct {
 	fd      int
 	w       io.Writer
-	r       io.Reader
+	r       *bufio.Reader
 	buf     bytes.Buffer
 	isTerm  bool
 	line    int
 	stopped bool
 }
 
+var _ io.Writer = &pager{}
+
 func newPager(r io.Reader, w io.Writer) *pager {
 	isTerm := false
-	fd := 0
+	fd := -1
 
-	f, ok := r.(*os.File)
+	f, ok := r.(interface{ Fd() uintptr }) // usually *os.File
 	if ok {
 		fd = int(f.Fd())
 		isTerm = term.IsTerminal(fd)
@@ -37,7 +37,7 @@ func newPager(r io.Reader, w io.Writer) *pager {
 	return &pager{
 		fd:     fd,
 		w:      w,
-		r:      r,
+		r:      bufio.NewReader(r),
 		buf:    bytes.Buffer{},
 		isTerm: isTerm,
 	}
@@ -60,65 +60,72 @@ func (p *pager) Write(b []byte) (n int, err error) {
 	p.buf.Write(b)
 
 	written := 0
-	for {
+	for written < len(b) {
 		if p.line >= height-1 {
-		ReadChar:
-			for {
-				fmt.Fprintf(p.w, "--More--")
-
-				b := make([]byte, 1)
-				_, err := p.r.Read(b)
-				if err != nil {
-					return written, err
-				}
-
-				fmt.Fprintf(p.w, "%s", "\r"+strings.Repeat(" ", len("--More--"))+"\r")
-
-				switch b[0] {
-				case 'q':
-					p.stopped = true
-					return written, io.EOF
-				case ' ':
-					p.line = 0
-					break ReadChar
-				case '\r', 'j':
-					p.line--
-					break ReadChar
-				case '\x1b':
-					// escape sequence, read next two bytes
-					b := make([]byte, 2)
-					_, err := p.r.Read(b)
-					if err != nil {
-						return written, err
-					}
-
-					if b[0] == '[' && b[1] == 'B' {
-						// down arrow
-						p.line--
-						break ReadChar
-					}
-				}
-
-				// invalid character, ring bell
-				fmt.Fprintf(p.w, "\a")
+			// we're at the bottom of the screen, so wait for user input
+			// before continuing. It's height-1 rather than height to
+			// leave room for "--More--".
+			err := p.paginate()
+			if err != nil {
+				return written, err
 			}
 		}
 
-		line, err := p.buf.ReadString('\n')
+		line, err := p.buf.ReadBytes('\n')
 		if err != nil && err != io.EOF {
 			return written, err
 		}
-
-		n, err = fmt.Fprintf(p.w, "%s", line)
+		n, err := p.w.Write(line)
 		written += n
 		if err != nil {
 			return written, err
 		}
-
 		p.line++
+	}
+	return written, nil
+}
 
-		if written == len(b) {
-			return written, nil
+func (p *pager) paginate() error {
+	more := []byte("--More--")
+	clear := []byte("\r" + strings.Repeat(" ", len(more)) + "\r")
+
+	for {
+		_, err := p.w.Write(more)
+		if err != nil {
+			return err
+		}
+
+		b, err := p.r.ReadByte()
+		if err != nil {
+			return err
+		}
+
+		_, err = p.w.Write(clear)
+		if err != nil {
+			return err
+		}
+
+		switch b {
+		case 'q':
+			p.stopped = true
+			return io.EOF
+		case ' ':
+			p.line = 0
+			return nil
+		case '\r', 'j':
+			p.line--
+			return nil
+		case '\x1b': // escape sequence, read next two bytes
+			var b [2]byte
+			_, err := p.r.Read(b[:])
+			if err != nil {
+				return err
+			}
+			if b[0] == '[' && b[1] == 'B' {
+				// down arrow
+				p.line--
+				return nil
+			}
 		}
 	}
 }
